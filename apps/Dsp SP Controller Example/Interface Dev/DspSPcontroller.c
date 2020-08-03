4d76fdbb26fc66029932d41f5ed69131cc2502d7
Robert Guzikowski <robert.guzikowski@AdestoTech.com>
2020-08-03 09:09:18 -0700

    Added Starter Kit folder for the initial release supporting 2.80.42.

--- apps/Dsp SP Controller Example/Interface Dev/DspSPcontroller.c
+++ apps/Dsp SP Controller Example/Interface Dev/DspSPcontroller.c
@@ -1,12 +1,15 @@
 /*
  * Interface definition using IML to define the Internal Application Example interface.
- * This file is the input to the III.exe application, to produce aN XIF file. You need
+ * This file is the input to the iii.exe application, to produce aN XIF file. You need
  * to place the ApolloDev.* resouce files in a location that matches the location
- * in the IML belowUDPTDspSPcontroller.
+ * in the IML belowUDPTDspSPcontroller. 
+ * *** Note iii.exe 1.30.009 or higher is required ***
  */
-//@Izot option target("shortstack-classic")
-//@Izot option server("SS430_FT6050_SYS20000kHz")
+//@Izot option target("xif")
 //@Izot option programId("90:00:01:06:00:03:85:00")
+//@IzoT option dynamic(blocks=0, datapoints=0) 
+//@IzoT option addresses(15)
+//@IzoT option aliases(15)
 
 UFPTDspSPcontroller(DspCntl) DspCntl; //@Izot block (location="./P9000010600000000_4") external("SpController") 
 
