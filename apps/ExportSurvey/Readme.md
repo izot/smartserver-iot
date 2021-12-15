e306d0bcd4acc10ef6d12c7c819eb03d8820a1c1
rguzikow <robert.guzikowski@diasemi.com>
2021-12-15 07:18:29 -0800

    exportSurvey app upgraded to 1.00.002.

--- apps/ExportSurvey/Readme.md
+++ apps/ExportSurvey/Readme.md
@@ -1,3 +1,35 @@
-# ExportSurvey Utility
+# ExportSurvey Utility (exportSurvey.exe)
 --- 
-This helper utility processes an XML Subsystem report to summarize the resources needed to fully represent the datapoints in a IzoT CT based network that is to be exported to the SmartServer IoT using `inex.exe`. Armed with this information, the network integrators can gather and pre-load resources to improve results migrating existing systems to the SmartServer IoT.
+This report utility processes XML Subsystem report to summarize the resources needed to fully represent the datapoints in a IzoT CT based network you intend to export to the SmartServer IoT using `inex.exe`. Armed with this information, you can gather and pre-load resources to improve results migrating existing systems to the SmartServer IoT.
+
+This application is a standalone executable that should be added to your `../lonworks/bin` folder.  
+
+## Instructions
+This workflow uses the SmartServer IoT in IMM mode.
+1. Prepare for the `inex` by creating a subsystem that will contain assets you want to export.
+2. In IzoT CT, move device shapes for assets you want to export to the subsystem created in the previous step.
+3. Add the smartserver internal device `SmartServerIoT.xif` and necessary routers and channels to the design.  
+4. In CT with the export subsystem as your current subsystem, Right-click the drawing page and launch the XML export plugin.
+
+![Plugin Launch](docImages/XMLexportPlugin.png)
+
+4. Select these options: 
+
+![XML Export Plugin Options](docImages/ExportPluginCfg.png)
+
+5. Use file explorer to navigate to the folder that contains the XML Export file.  Type `cmd` in the file explorer address box to launch a command window with the export file folder as the current working directory.
+6. Type: `exportSurvey ./Export.xml`
+
+Here is an example of the output generated (exportReport.csv) by this utility.  
+
+![ExportSurvey Report](docImages/exportSurvey%20Report.png)
+
+The exportSurvey also generates the file `deviceTypes.dtd` in the same folder.  You can add this to a .zip archive that includes the XIF files to create a device type package that can be imported by the Device Type widget in the CMS.  Once imported, you can use the `/skipfiles` option when running `inex.exe` 
+
+## Release Notes
+***
+**12/15/2021 - Release 1.00.002**: 
+1. Fixed issues with file path handling.
+2. Fixed bugs found when working with more complex actual projects.
+3. Added the dtd file generation feature. 
+
--- apps/ExportSurvey/exportSurvey.js
+++ apps/ExportSurvey/exportSurvey.js
@@ -2,12 +2,18 @@
 let fs = require('fs');
 let xml2js = require('xml2js');
 const mqtt = require('mqtt');
+const { cpuUsage } = require('process');
 
-const appVersion = '1.00.001';
+const appVersion = '1.00.002';
 // File: exportSurvey.js
 // Input file is the output of an IzoT CT XML export for an existing IzoT CT project. This utility
-// will summerize the resources that need to be added to the SmartServer IoT prior to running
-// inex.exe to create IAP accesssable datapoint that use custome resources.
+// will summarize the resources that need to be added to the SmartServer IoT prior to running
+// inex.exe to create IAP accessible datapoint that use custom resources.
+// 
+// 12/14/2021: Release 1.00.002
+//   fixed path issues for input file.
+//   Fixed bugs discovered in more complex device definitions from NYC example db
+//   Added support for dtd file generation.
 
 let parser = new xml2js.Parser({explicitArray:false});       
 
@@ -36,6 +42,7 @@ if (args.length >= 1) {
 
 let AppDevCollection = new Array();
 let templateCollection = new Map();
+let userTypesSet = new Set();
 let connectionCount = 0;
 
 function pushDevice (AppDev) {
@@ -58,8 +65,8 @@ function pushTemplate (devTemplate) {
             AppDevCollection.push(appD); 
     return;
 }
-console.log(`Data file: ${__dirname}\\${fileName}`);
-fs.readFile(__dirname + `\\${fileName}`, function(err, data) {
+console.log(`Data file: ${fileName}`);
+fs.readFile(`${fileName}`, function(err, data) {
     if (err) {
         console.log(`Failed to open Export file: ${err}`);
         process.exit(0);
@@ -71,7 +78,7 @@ fs.readFile(__dirname + `\\${fileName}`, function(err, data) {
         let tmplRoot = result.LonWorksNetwork.DeviceTemplates;
         for (let devTemplate of tmplRoot.DeviceTemplate ) {
             templateCollection.set(devTemplate.Name, devTemplate);
-            console.log (`${devTemplate.Name}, XIF path: ${devTemplate.XifPath}`);
+            console.log (`${devTemplate.Name}, XIF path: ${devTemplate.XifPath =='' ? 'addhoc':devTemplate.XifPath}`);
         }
         // Creating a flat collecitons of Devices
         // Handle the signal subsystem export and root level AppDevices
@@ -109,26 +116,33 @@ fs.readFile(__dirname + `\\${fileName}`, function(err, data) {
             console.log(`No application devices are defined in the file ${fileName}`);
             return;
         }
-        let thisHndl;
-        let reportS = fs.createWriteStream(__dirname + `/exportReport.csv`);
-        reportS.write(`LNS Template,PID,XIF Path,Custom Resoure,DevFb\n`);
-        //conFileS.write('Rule ID,Area Scope,Zone Scope,Device Name,Device Type,Datapoint Name,Direction,Map\n');
-        //for (let devTempl)
-        for (let appDev of AppDevCollection) {
 
+        let reportS = fs.createWriteStream(`./exportReport.csv`);
+        let dtdStream = fs.createWriteStream(`./deviceTypes.dtd`);
+        dtdStream.write('#filetype,dtd\n');
+        dtdStream.write(`\"Device Type\",Protocol,\"Program ID\",\"Default App\",\"Default Sys\",\"Auto App Load\",\"Auto Sys Load\",\"Graphics File\",Default\n`);
+        reportS.write(`LNS Template,PID,XIF Path,Custom Resource,DevFb\n`);
+
+        for (let appDev of AppDevCollection) {
+            let reportRowStr;
             if (!appDev.hasOwnProperty('NeuronId'))
                 continue; 
             let devTmp = templateCollection.get(appDev.Template);
  
             //let fbCollection = appDev.FunctionalBlocks.FunctionalBlock.length == null ? [appDev.FunctionalBlocks.FunctionalBlock] : appDev.FunctionalBlocks.FunctionalBlock;
-            let fbs = appDev.FunctionalBlocks;
+            //let fbs = appDev.FunctionalBlocks;
+            if (devTmp == null)
+                continue;
+            let fbs = devTmp.FunctionalBlocks;
             if (fbs == null || typeof(fbs) == 'string')
                 continue;
             if (devTmp != null) {
-                reportS.write(`${devTmp.Name},${devTmp.Classification.ProgramId},${devTmp.XifPath},`)  
+                dtdStream.write(`${devTmp.Name},lon,${devTmp.Classification.ProgramId},,,false,false,,true\n`);
+                reportRowStr = `${devTmp.Name},\'${devTmp.Classification.ProgramId}\',${devTmp.XifPath =='' ? 'addhoc':devTmp.XifPath},`;
             } else {
                 continue;
-            }              let fbCollection = appDev.FunctionalBlocks.FunctionalBlock;
+            }              
+            let fbCollection = devTmp.FunctionalBlocks.FunctionalBlock;
             // Need to promote an object to an array for Interfaces with a single FB.
             if (fbCollection.$ != null)
                 fbCollection = [fbCollection];
@@ -140,42 +154,61 @@ fs.readFile(__dirname + `\\${fileName}`, function(err, data) {
                 let fbName;
                 let fbProgName;
                 let fbIndex;
+                if (!fb.hasOwnProperty('NetworkVariables'))
+                    continue;
                 let nvs = fb.NetworkVariables.NetworkVariable;
-
                 if (nvs == null) 
                     continue;
                 if (fb.hasOwnProperty('ConfigProperties')) {
                     let cps = fb.ConfigProperties.ConfigProperty;
-                    cps.forEach(cp => {
-                        if(cp.Name.includes('UCPT'))
+                    if (cps == null || cps.length == 0)
+                        continue;
+                    if (cps.hasOwnProperty('Name')) {
+                        if(cps.Name.includes('UCPT')) {
                             ++ucptCount;
-                        }  
-                    );
+                            userTypesSet.add(cps.Name);
+                        };
+                        continue;
+                    }    
+                    cps.forEach(cp => {
+                        if(cp.Name.includes('UCPT')) {
+                            ++ucptCount;
+                            userTypesSet.add(cp.Name);
+                        }
+                    });
                 };    
                 fbProgName = fb.IsVirtualFb._ == 'True' ? 'device' : fb.ProgrammaticName.replace(/[^a-z,^A-Z]/g,'');    
                 fbName = fb.Name;
                 for (let m = 0; m < nvs.length; m++) {
                     let nv = nvs[m];
-                    if (nv.Format.includes('UNVT')) {
+                    if (nv.TypeSpec.TypeName.includes('UNVT')) {
                         ++unvtCount;
-                        if (fb.IsVirtualFb._ == 'True')
+                        if (fb.IsVirtualFb._ == 'True') {
                             ++unvtDevCount; 
+                        }
+                        userTypesSet.add(nv.TypeSpec.TypeName);
                     }
                     if (nv.hasOwnProperty('ConfigProperty')) {
                         let nvcps = nv.ConfigProperties.ConfigProperty;
                         nvcps.forEach(nvcp => {
-                            if(cp.Name.includes('UCPT'))
+                            if(cp.Name.includes('UCPT')) {
                                 ++ucptCount;
+                                userTypesSet.add(cp.Name);
+                            }
                         })
                     }
                 }    
-
             }
             //reportS.write(`LNS Template,PID,XIF Path,Custom Resoure,DevFb\n`);
-            reportS.write(`${unvtCount},${unvtDevCount},${ucptCount}\n`);
+            //`LNS Template,PID,XIF Path,Custom Resource,DevFb
+            reportRowStr += `${userTypesSet.size},${unvtDevCount}`;
+            console.log (reportRowStr);
+            reportS.write(reportRowStr + '\n');
+            userTypesSet.clear();
             //templateCollection.delete(appDev.Template);
         }
         reportS.end();
+        dtdStream.end();
     });
     //console.dir(result);
 
